9eb2a71ec57289cd4daa91c692cb09ea1a109041
Mikael M <mikael_96@live.com>
2017-11-22 20:52:32 +0200

    putting all the files to new location

---
+++ BCW2-master/BCW2-master/js/main-c.js
@@ -0,0 +1,57 @@
+'use strict';
+// Create function 'showImages' which
+// loads images.json which has links to images as an array.
+
+// create a loop which builds the following HTML from every image in the array: 
+/*
+<li>
+    <figure>
+        <a href="img/original/filename.jpg"><img src="img/thumbs/filename.jpg"></a>
+        <figcaption>
+            <h3>Title</h3>
+        </figcaption>
+    </figure>
+</li>
+*/
+// Make the above HTML by using DOM methods.
+// Create elements with createElement()
+// Add attributes with setAttribute()
+// Add elements with appendChild
+// When the above HTML is ready append it to the <ul> element
+
+const showImages = () => {
+    const ul = document.querySelector('ul');
+
+    fetch('images.json')
+        .then((response) => {
+            return response.json();
+        })
+        .then((json) => {
+            json.forEach((image) => {
+                //put code here
+                const li = document.createElement('li');
+                const figure = document.createElement('figure');
+                const a = document.createElement('a');
+                const figcaption = document.createElement('figcaption');
+                const h3 = document.createElement('h3');
+                const img = document.createElement('img');
+
+                const headline = document.createTextNode(image.mediaTitle);
+
+                h3.appendChild(headline);
+                figcaption.appendChild(h3);
+
+                img.setAttribute('src', `img/thumbs/${image.mediaUrl}`);
+                a.setAttribute('href', `img/original/${image.mediaUrl}`);
+
+                a.appendChild(img);
+                figure.appendChild(a);
+                figure.appendChild(figcaption);
+                li.appendChild(figure);
+                ul.appendChild(li);
+
+            });
+        });
+};
+
+showImages();
---
+++ BCW4-master/BCW4-master/js/main-a.js
@@ -0,0 +1,34 @@
+'use strict';
+//select buttons
+const addButton = document.querySelector('#add');
+const changeButton = document.querySelector('#change');
+const toggleButton = document.querySelector('#toggle');
+
+
+//select p elements
+const firstP= document.querySelector('p');
+const secondP = document.querySelector('p:nth-child(2)');
+const thirdP = document.querySelector('p:nth-child(3)');
+
+//create eventListeners
+
+addButton.addEventListener('click', (evt) => {
+    firstP.classList.add('red');
+});
+
+
+changeButton.addEventListener('click', (evt) => {
+    //if secondP contains class red
+    if(secondP.classList.contains('red')){
+        //replace it with 'blue'
+        secondP.classList.replace('red', 'blue');
+    }//else
+    else {
+        //replace with 'red'
+        secondP.classList.replace('blue', 'red');
+    }
+});
+
+toggleButton.addEventListener('click', (evt) => {
+    thirdP.classList.toggle('green');
+});
